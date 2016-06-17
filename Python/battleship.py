9fe896be996aab58496cbd16f1cca720e6ee949a
diggerydoo68 <rammerjam64@gmail.com>
2016-06-17 11:05:19 -0600

    Update battleship.py

--- Python/battleship.py
+++ Python/battleship.py
@@ -1,19 +1,52 @@
-from random import randint 
+from random import randint
 
 board = []
 
-for x in range(0, 5):
+for x in range(5):
     board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
         print " ".join(row)
 
-# Add your code below!
+print "Let's play Battleship!"
+print_board(board)
+
 def random_row(board):
-    return randint( 0, len(board) - 1)
+    return randint(0, len(board) - 1)
+
 def random_col(board):
-    return randint( 0, len(board[0]) - 1)
-    
-print "Row Number: ", random_row(board)
-print "Column Number: ", random_col(board)
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+# print ship_row
+# print ship_col
+
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+turn = 5 #set turn 
+while True:
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!" # print before you break
+        break
+
+    #I want to put my break statement here, but how?
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            turn-=1  # decrease turn if player misses
+            board[guess_row][guess_col] = "X"
+    # Print (turn + 1) here!
+        print_board(board)
+        if turn ==0:
+            print "Game Over"
+            break
